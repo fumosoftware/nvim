11310fb7afa7b1a5a1efaffa0becfd6be5ead492
fumosoftware <projectacorn@fumosoft.com>
2025-02-03 16:13:27 -0600

    added neotree and setup lazy package manager keybindings

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -23,11 +23,26 @@ return {
 	  })
 
 	  wk.add({
-		  { "<leader>L", "<cmd>Lazy<cr>", desc = "Lazy", mode = "n" },
+		  { "<leader>L", group = "Lazy" },
+		  { "<leader>L?", "<cmd>Lazy help<cr>", desc = "Lazy Help", mode = "n" },
+		  { "<leader>LH", "<cmd>Lazy<cr>", desc = "Lazy Home", mode = "n" },
+		  { "<leader>LD", "<cmd>Lazy debug<cr>", desc = "Lazy Debug", mode = "n" },
+		  { "<leader>LL", "<cmd>Lazy log<cr>", desc = "Lazy Log", mode = "n" },
+		  { "<leader>LC", "<cmd>Lazy check<cr>", desc = "Lazy Check", mode = "n" },
+		  { "<leader>LI", "<cmd>Lazy install<cr>", desc = "Lazy Install", mode = "n" },
+		  { "<leader>LU", "<cmd>Lazy update<cr>", desc = "Lazy Update", mode = "n" },
+		  { "<leader>LS", "<cmd>Lazy sync<cr>", desc = "Lazy Sync", mode = "n" },
+		  { "<leader>LR", "<cmd>Lazy restore<cr>", desc = "Lazy Restore", mode = "n" },
+		  { "<leader>LP", "<cmd>Lazy profile<cr>", desc = "Lazy Profile", mode = "n" },
+		  { "<leader>LX", "<cmd>Lazy clean<cr>", desc = "Lazy Clean", mode = "n" },
 	  })
 
 	  wk.add({
 		  { "<leader>z", function() require("zen-mode").toggle( { window = { width = .85 } } ) end, desc = "Zen", mode = "n" },
+	  })
+
+	  wk.add({
+		  { "<leader>\\", "<cmd>Neotree filesystem toggle right<cr>", desc = "File Tree", mode = "n" } ,
 	  })
   end,
 }

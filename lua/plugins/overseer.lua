fcbf41be4908401d1b8e2120ed68af82bc0f0a58
fumosoftware <projectacorn@fumosoft.com>
2025-02-14 13:55:26 -0600

    updated overseer tasks

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -2,8 +2,9 @@ return {
   'stevearc/overseer.nvim',
   config = function()
     local overseer = require("overseer")
-    overseer.setup({})
-
+    overseer.setup({
+			templates = { "builtin", "cmake.cmake_presets" },
+    })
 
     overseer.add_template_hook({ module = "vscode" }, function(task_defn,util)
 		util.add_component(task_defn, { "vscode_utils.restart_lsp" })
